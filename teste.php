4beb05679788d813c202a865d255c113ec20a870
Jefferson Lima <jefferson.carrenho@gmail.com>
2015-08-27 16:04:15 -0300

    Aula2

--- teste.php
+++ teste.php
@@ -1,9 +1,19 @@
 <?php
 //Arquivo para testes
-    require_once("classes/teste.class.php");
+require_once("classes/teste.class.php");
+require_once("classes/clientes.class.php");
+//$testando = new Teste();
+//$cliente = new Clientes();
+//$cliente->setValor('nome', 'Jeff');
+//$cliente->setValor('sobrenome', 'Lima');
+//$cliente->setValor('bairro', 'Centro');
 
-$testando = new Teste();
+//$cliente->delCampo('sobrenome');
+//$cliente->addCampo('bairro', 'bairro tal');
+/*echo "<pre>";
+    print_r($cliente);
+echo "</pre>";*/
 
-
+echo ($cliente->getValor('nome'));
 
 ?>
