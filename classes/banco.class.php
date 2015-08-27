34b50d3c7d8d56bff6309c994b0193413ce91e56
Jefferson Lima <jefferson.carrenho@gmail.com>
2015-08-27 17:26:43 -0300

    Aula 3

--- classes/banco.class.php
+++ classes/banco.class.php
@@ -30,6 +30,43 @@ abstract class Banco{
         mysql_query("SET character_set_result=utf8");
     }//conecta
     
+    public function inserir ($objeto){
+        //insert into nome_da_tabela (campo1, campo2) Values (valor1, valor2)
+        $sql = "INSERT INTO " . $objeto->tabela. " (";
+        for($i=0; $i<count($objeto->campos_valores); $i++):
+            $sql .= key($objeto->campos_valores);
+            if ($i < (count($objeto->campos_valores)-1)):
+                $sql.=", ";
+            else:    
+                $sql.=")";
+            endif;
+            next($objeto->campos_valores);
+        endfor;
+        reset($objeto->campos_valores);
+        $sql .= " Values (";
+        for($i=0; $i<count($objeto->campos_valores); $i++):
+            $sql .= is_numeric($objeto->campos_valores[key($objeto->campos_valores)]) ? $objeto->campos_valores[key($objeto->campos_valores)] : "'".$objeto->campos_valores[key($objeto->campos_valores)]."'";
+            if ($i < (count($objeto->campos_valores)-1)):
+                $sql.=", ";
+            else:    
+                $sql.=")";
+            endif;
+            next($objeto->campos_valores);
+        endfor;
+        echo $sql;
+        return $this->executaSQL($sql);
+        $this->linhasafetadas = mysql_affected_rows($this->conexao);
+        
+    }//fim inserir
+    
+    public function executaSQL($sql = NULL){
+        if($sql != null):
+            $query = mysql_query($sql) or $this->trataerro(__FILE__, __FUNCTION__);
+        else:
+            $this->trataerro(__FILE__, __FUNCTION__, NULL, ' Comando SQL nao informado na rotina', FALSE);
+        endif;
+    }//fim executaSQL
+    
     public function trataerro($arquivo=NULL, $rotina=NULL, $numerro=NULL, $msgerro=NULL, $geraexcept=FALSE){
         if($arquivo == NULL) $arquivo = "Nao informado";
         if($rotina == NULL) $rotina = "Nao informada";
@@ -45,6 +82,6 @@ abstract class Banco{
         else:
             die($resultado);
         endif;        
-    }//trataerro
+    }//trataaerro
 }//fim classe banco
 ?>
--- teste.php
+++ teste.php
@@ -3,4 +3,15 @@
 require_once("classes/teste.class.php");
 require_once("classes/clientes.class.php");
 $cliente = new Clientes();
+$cliente->setValor("nome", "Jefferson");
+$cliente->setValor("sobrenome", "Lima");
+$cliente->inserir($cliente);
+echo "<br />";
+//echo $cliente->linhasafetadas. ' Registro(s) incluido(s) com sucesso';
+
 ?>
+
+
+<pre>
+    <?php print_r($cliente);?>
+</pre>
